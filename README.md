99d971b853198fe9940f0455e26ec2a56bba8788
Anthony Beaver <anthonybeavermusic@gmail.com>
2020-07-25 18:45:20 -0700

    Lab:03 updated about me

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # 201n19-class-02-demo
+
+7-25: UPDATE to this file for Lab:03
+I will now be adding my top 10 at the bottome of the HTML page as an ordered list.
+I'm adding a 6th question that takes a numeric input and indicates if the answer is too high or low. It will give the user exactly 4 opportunities. If all are incorrect it will tell the user the right answer.
+I'm also adding a 7th question which will be multiple choice with all answers shown at the prompt. It will have the answers stored in an array. 6 chances to get this one right. The guesses will stop once the user is correct or they run out of choices. 
+
+I'll be implementing a loop for question 6 and 7.
+
+At the end i will tell them how many answers they got correct.
--- js/app.js
+++ js/app.js
@@ -9,10 +9,12 @@
 // Remember to add commit push to my repo in github
 // submit the url of your repo to the assignment
 
+var totalScore = 0;
+
 var userName = prompt('Welcome to my guessing game site! Before we start lets get your name:');
 console.log(userName);
 
-var yesNo = confirm('Hi ' + userName + '! You have just entered the guessing game! Please answer all questions with either yes/no or with y/n. If you\'re feeling crazy try a random word and see what happens! Now let\'s begin.');
+var yesNo = confirm('Hi ' + userName + '! You have just entered the guessing game! Please answer all questions with either yes/no or with y/n. If you\'re feeling crazy try a random //word and see what happens! Now let\'s begin.');
 console.log(yesNo);
 
 //First Question
@@ -23,6 +25,8 @@ console.log(lowerOne);
 if (lowerOne === 'yes' || lowerOne === 'y') {
   //console.log('It was true');
   alert('Correct!');
+   totalScore++
+   console.log(`current score ${totalScore++}`)
 } 
 else if (lowerOne === 'no' || lowerOne === 'n') {
   //console.log('It was false');
@@ -45,6 +49,7 @@ if (lowerTwo === 'yes' || lowerTwo === 'y') {
 else if (lowerTwo === 'no' || lowerTwo === 'n') {
   //console.log('It was false');
   alert('Correct!');
+  totalScore++
 } 
 else {
   //console.log('Not a good answer');
@@ -59,6 +64,7 @@ console.log(lowerThree);
 if (lowerThree === 'yes' || lowerThree === 'y') {
   //console.log('It was true');
   alert('Correct!')
+  totalScore++
 } 
 else if (lowerThree === 'no' || lowerThree === 'n') {
   //console.log('It was false');
@@ -77,6 +83,7 @@ console.log(lowerFour);
 if (lowerFour === 'yes' || lowerFour === 'y') {
   //console.log('It was true');
   alert('Correct!')
+  totalScore++
 } 
 else if (lowerFour === 'no' || lowerFour === 'n') {
   //console.log('It was false');
@@ -99,11 +106,80 @@ if (lowerFive === 'yes' || lowerFive === 'y') {
 else if (lowerFive === 'no' || lowerFive === 'n') {
   //console.log('It was false');
   alert('Correct!');
+  totalScore++
 } 
 else {
   //console.log('Not a good answer');
   alert('Not a yes or no answer!');
 }
 
+var numTime = confirm('Let\'s switch it up! The next question is looking for a number instead of a yes/no or y/n answer.')
+console.log(`${numTime}`);
+
+// Sixth Question
+var answerSix = parseInt(prompt('One plus one is: '));
+//console.log(typeof answerSix)
+console.log(`Answer six is ${answerSix}`)
+var guessCount = 0;
+
+while (guessCount < 6) {
+  console.log(`Answer six2 is ${answerSix}`)
+  //console.log(`Current ${guessCount}`)
+  var numAnswer = 2
+    if (answerSix === numAnswer) {
+      alert('You got it!');
+      totalScore++
+      console.log(`Score is ${totalScore}.`)
+      break;
+    }
+    else if (answerSix > numAnswer) {
+      alert('Too high!');
+      guessCount++
+      console.log(`Current ${guessCount}`)
+      answerSix = parseInt(prompt('One plus one is: '));
+    }
+    else if (answerSix < numAnswer) {
+      alert('Too low!');
+      guessCount++
+      console.log(`Current ${guessCount}`)
+      answerSix = parseInt(prompt('One plus one is: '));
+    }
+    if (guessCount === 5) {
+      alert(`Too many guesses, the answer is ${numAnswer}`);
+      break; 
+    }
+}
+
+confirm('Okay, time for a multipe choice question! Please answer with either A, B, C or D')
+//Question Seven
+var answerSeven = prompt('Which of the following are sea-dwelling dinosaurs? A) Argentinosaurus, B) Ammonite, C) Megalodon, D) All of the above!');
+var upperSeven = answerSeven.toUpperCase();
+console.log(upperSeven)
+var sevenAnswerArray = ['B', 'C', 'D'];
+var mGuessCount = 0
+
+while (mGuessCount < 7) {
+  console.log(answerSeven);
+  console.log(upperSeven);
+  if (upperSeven !== sevenAnswerArray[0] && upperSeven !== sevenAnswerArray[1] && upperSeven !== sevenAnswerArray[2]) {
+    alert(`Sorry that was incorrect. Try again!`)
+    answerSeven = prompt('Which of the following are sea-dwelling dinosaurs? A) Argentinosaurus, B) Ammonite, C) Megalodon, D) All of the above!')
+    upperSeven = answerSeven.toUpperCase();
+    mGuessCount++
+    console.log(`Current Count ${mGuessCount}`)
+  }
+  else {
+    console.log(`${upperSeven} is correct!`);
+    alert(`That is correct! the answers are B) Ammonite, C) Megalodon, D) All of the above! `);
+    totalScore++
+    break;
+  }
+  if (mGuessCount === 6) {
+    alert(`Too many guesses. The answers were ${sevenAnswerArray[0]}, ${sevenAnswerArray[1]}, ${sevenAnswerArray[2]}.`);
+    break;
+  }
+
+}
+
 //Final Message to User
-alert("Okay, " + userName + " the results are in! You answered " + lowerOne + ", " + lowerTwo + ", " + lowerThree + ", " + lowerFour + " and " + lowerFive + ". Thanks for answering all questions! If you got any wrong and would like to retry, simply refresh the page.");
+alert(`Okay, ${userName} the results are in! You answered ${lowerOne}, ${lowerTwo}, ${lowerThree},${lowerFour}, ${lowerFive}, ${answerSix} and ${upperSeven}. Thanks for answering all questions! Your total score was ${totalScore} our 7. If you got any wrong and would like to retry, simply refresh the page."`);
