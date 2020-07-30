a51a5a437c85befc7782a0dc22b40fb0d54b43eb
Anthony Beaver <anthonybeavermusic@gmail.com>
2020-07-29 18:14:03 -0700

    updated the js and the html

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ var totalScore = 0;
 var userName = prompt('Welcome to my guessing game site! Before we start lets get your name:');
 console.log(userName);
 
-var yesNo = confirm('Hi ' + userName + '! You have just entered the guessing game! Please answer all questions with either yes/no or with y/n. If you\'re feeling crazy try a random //word and see what happens! Now let\'s begin.');
+var yesNo = confirm('Hi ' + userName + '! You have just entered the guessing game! Please answer all questions with either yes/no or with y/n. If you\'re feeling crazy try a random word and see what happens! Now let\'s begin.');
 console.log(yesNo);
 
 //First Question
@@ -25,13 +25,13 @@ console.log(lowerOne);
 if (lowerOne === 'yes' || lowerOne === 'y') {
   //console.log('It was true');
   alert('Correct!');
-   totalScore++
-   console.log(`current score ${totalScore++}`)
-} 
+  totalScore++;
+  console.log(`current score ${totalScore++}`);
+}
 else if (lowerOne === 'no' || lowerOne === 'n') {
   //console.log('It was false');
   alert('False!');
-} 
+}
 else {
   //console.log('Not a good answer');
   alert('Not a yes or no answer!');
@@ -49,8 +49,8 @@ if (lowerTwo === 'yes' || lowerTwo === 'y') {
 else if (lowerTwo === 'no' || lowerTwo === 'n') {
   //console.log('It was false');
   alert('Correct!');
-  totalScore++
-} 
+  totalScore++;
+}
 else {
   //console.log('Not a good answer');
   alert('Not a yes or no answer!');
@@ -63,32 +63,32 @@ console.log(lowerThree);
 //Yes answer
 if (lowerThree === 'yes' || lowerThree === 'y') {
   //console.log('It was true');
-  alert('Correct!')
-  totalScore++
-} 
+  alert('Correct!');
+  totalScore++;
+}
 else if (lowerThree === 'no' || lowerThree === 'n') {
   //console.log('It was false');
   alert('False!');
-} 
+}
 else {
   //console.log('Not a good answer');
   alert('Not a yes or no answer!');
 }
 
 //Fourth Question
-var answerFour = prompt('Can a cat jump 6 times it\s length?');
+var answerFour = prompt('Can a cat jump 6 times it\'s length?');
 var lowerFour = answerFour.toLowerCase();
 console.log(lowerFour);
 //Yes Answer
 if (lowerFour === 'yes' || lowerFour === 'y') {
   //console.log('It was true');
-  alert('Correct!')
-  totalScore++
-} 
+  alert('Correct!');
+  totalScore++;
+}
 else if (lowerFour === 'no' || lowerFour === 'n') {
   //console.log('It was false');
   alert('False!');
-} 
+}
 else {
   //console.log('Not a good answer');
   alert('Not a yes or no answer!');
@@ -101,85 +101,111 @@ console.log(lowerFive);
 //No Answer
 if (lowerFive === 'yes' || lowerFive === 'y') {
   //console.log('It was false');
-  alert('False!')
+  alert('False!');
 }
 else if (lowerFive === 'no' || lowerFive === 'n') {
   //console.log('It was false');
   alert('Correct!');
-  totalScore++
-} 
+  totalScore++;
+}
 else {
   //console.log('Not a good answer');
   alert('Not a yes or no answer!');
 }
 
-var numTime = confirm('Let\'s switch it up! The next question is looking for a number instead of a yes/no or y/n answer.')
+var numTime = confirm('Let\'s switch it up! The next question is looking for a number instead of a yes/no or y/n answer.');
 console.log(`${numTime}`);
 
 // Sixth Question
 var answerSix = parseInt(prompt('One plus one is: '));
 //console.log(typeof answerSix)
-console.log(`Answer six is ${answerSix}`)
+console.log(`Answer six is ${answerSix}`);
 var guessCount = 0;
+var allowedAttempts = 4;
 
-while (guessCount < 6) {
-  console.log(`Answer six2 is ${answerSix}`)
+while (guessCount < allowedAttempts) {
+  console.log(`Answer six2 is ${answerSix}`);
   //console.log(`Current ${guessCount}`)
-  var numAnswer = 2
-    if (answerSix === numAnswer) {
-      alert('You got it!');
-      totalScore++
-      console.log(`Score is ${totalScore}.`)
-      break;
-    }
-    else if (answerSix > numAnswer) {
-      alert('Too high!');
-      guessCount++
-      console.log(`Current ${guessCount}`)
-      answerSix = parseInt(prompt('One plus one is: '));
-    }
-    else if (answerSix < numAnswer) {
-      alert('Too low!');
-      guessCount++
-      console.log(`Current ${guessCount}`)
-      answerSix = parseInt(prompt('One plus one is: '));
-    }
-    if (guessCount === 5) {
-      alert(`Too many guesses, the answer is ${numAnswer}`);
-      break; 
-    }
+  var numAnswer = 2;
+  if (answerSix === numAnswer) {
+    alert('You got it!');
+    totalScore++;
+    console.log(`Score is ${totalScore}.`);
+    break;
+  }
+  else if (answerSix > numAnswer) {
+    alert('Too high!');
+    guessCount++;
+    console.log(`Current ${guessCount}`);
+    answerSix = parseInt(prompt('One plus one is: '));
+  }
+  else if (answerSix < numAnswer) {
+    alert('Too low!');
+    guessCount++;
+    console.log(`Current ${guessCount}`);
+    answerSix = parseInt(prompt('One plus one is: '));
+  }
+  if (guessCount === allowedAttempts) {
+    alert(`Too many guesses, the answer is ${numAnswer}`);
+    break;
+  }
 }
 
-confirm('Okay, time for a multipe choice question! Please answer with either A, B, C or D')
+confirm('Okay, time for a multipe choice question! Please answer with either A, B, C or D');
 //Question Seven
 var answerSeven = prompt('Which of the following are sea-dwelling dinosaurs? A) Argentinosaurus, B) Ammonite, C) Megalodon, D) All of the above!');
 var upperSeven = answerSeven.toUpperCase();
-console.log(upperSeven)
+//console.log(upperSeven);
 var sevenAnswerArray = ['B', 'C', 'D'];
-var mGuessCount = 0
+var mGuessCount = 0;
+var mAllowedAttempts = 6;
 
-while (mGuessCount < 7) {
-  console.log(answerSeven);
-  console.log(upperSeven);
+while (mGuessCount < mAllowedAttempts) {
+  //console.log(answerSeven);
+  //console.log(upperSeven);
   if (upperSeven !== sevenAnswerArray[0] && upperSeven !== sevenAnswerArray[1] && upperSeven !== sevenAnswerArray[2]) {
-    alert(`Sorry that was incorrect. Try again!`)
-    answerSeven = prompt('Which of the following are sea-dwelling dinosaurs? A) Argentinosaurus, B) Ammonite, C) Megalodon, D) All of the above!')
+    alert('Sorry that was incorrect. Try again!');
+    answerSeven = prompt('Which of the following are sea-dwelling dinosaurs? A) Argentinosaurus, B) Ammonite, C) Megalodon, D) All of the above!');
     upperSeven = answerSeven.toUpperCase();
-    mGuessCount++
-    console.log(`Current Count ${mGuessCount}`)
+    mGuessCount++;
+    console.log(`Current Count ${mGuessCount}`);
   }
   else {
     console.log(`${upperSeven} is correct!`);
-    alert(`That is correct! the answers are B) Ammonite, C) Megalodon, D) All of the above! `);
-    totalScore++
+    alert('That is correct! the answers are B) Ammonite, C) Megalodon, D) All of the above!');
+    totalScore++;
     break;
   }
-  if (mGuessCount === 6) {
+  if (mGuessCount === mAllowedAttempts) {
     alert(`Too many guesses. The answers were ${sevenAnswerArray[0]}, ${sevenAnswerArray[1]}, ${sevenAnswerArray[2]}.`);
     break;
   }
 
 }
 
+
+
+//different way to do the while loop and it works!
+// var answeredCorrectly = false;
+// while(mAllowedAttempts > 0 && !answeredCorrectly){
+//   mAllowedAttempts--;
+//   for(var i = 0; i < sevenAnswerArray.length; i++){
+//     //console.log(sevenAnswerArray[i]);
+//     if(upperSeven === sevenAnswerArray[i]){
+//       alert(`${userName}, great job that is the correct answer!`);
+//       totalScore++;
+//       answeredCorrectly = true;
+//     }
+//   }
+//   if(mAllowedAttempts > 0 && !answeredCorrectly){
+//     answerSeven = prompt('Sorry, guess again!');
+//     upperSeven = answerSeven.toUpperCase();
+//   }
+// }
+
+// if (mAllowedAttempts === 0 && !answeredCorrectly) {
+//   alert('Thanks for playing, out of guesses here are the correct answers.')
+// }
+
 //Final Message to User
-alert(`Okay, ${userName} the results are in! You answered ${lowerOne}, ${lowerTwo}, ${lowerThree},${lowerFour}, ${lowerFive}, ${answerSix} and ${upperSeven}. Thanks for answering all questions! Your total score was ${totalScore} our 7. If you got any wrong and would like to retry, simply refresh the page."`);
+alert(`Okay, ${userName} the results are in! You answered ${lowerOne}, ${lowerTwo}, ${lowerThree}, ${lowerFour}, ${lowerFive}, ${answerSix} and ${upperSeven}. Thanks for answering all questions! Your total score was ${totalScore} our 7. If you got any wrong and would like to retry, simply refresh the page.`);
